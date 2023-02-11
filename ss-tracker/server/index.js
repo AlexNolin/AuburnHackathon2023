71e483b5399e60355b1923c47f6b60cd9b3122b3
eris-he <foreverphysics7@gmail.com>
2023-02-11 16:57:24 -0600

    restructuring and making environment work

--- ss-tracker/server/index.js
+++ ss-tracker/server/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
+// const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// mongoose.connect('mongodb+srv://cluster0.fzlfou5.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
+//   {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+//   }
+// );
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//   console.log("Connected successfully");
+// });
+
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
