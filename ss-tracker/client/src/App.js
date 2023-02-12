bea9a100cb88ea5b7f8c34b8ea4e27263df592e0
eris-he <foreverphysics7@gmail.com>
2023-02-11 22:28:35 -0600

    front end work

--- ss-tracker/client/src/App.js
+++ ss-tracker/client/src/App.js
@@ -1,8 +1,11 @@
 import React from "react"; 
+import "./App.css";
+import Header from "./Content/Header";
 
 const App = () => {
     return(
         <div>
+            <Header />
             <h1>
                 App
             </h1>
