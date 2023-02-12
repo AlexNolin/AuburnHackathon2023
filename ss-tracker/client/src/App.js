7a393c2abdd68b8b67e84e4267e85318b828fa9e
eris-he <foreverphysics7@gmail.com>
2023-02-12 06:34:27 -0600

    more frontend work

--- ss-tracker/client/src/App.js
+++ ss-tracker/client/src/App.js
@@ -3,6 +3,9 @@ import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import './App.css';
 import TestData from './TestData';
 import Header from './Content/Header';
+import Map from './Content/Map';
+import Sidebar from './Content/Sidebar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 // const containerStyle = {
@@ -21,9 +24,20 @@ import Header from './Content/Header';
   const App = () => {
     return(
         <div>
-          <Header />
           <div>
-            <TestData />
+            <header>
+            <link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+              integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+              crossorigin="anonymous"
+            />
+            </header>
+          </div>
+          <Header />
+          <div class="row">
+            <Map />
+            <Sidebar />
           </div>
         </div>
     )
--- ss-tracker/client/src/index.js
+++ ss-tracker/client/src/index.js
@@ -2,6 +2,8 @@ import React, { StrictMode } from 'react';
 import { createRoot } from "react-dom/client";
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const rootElement = document.getElementById('root');
