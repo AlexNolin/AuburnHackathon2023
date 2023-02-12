6ee441fb81736a3c6a86c1ea0ed4c0595de51432
Alexander V Nolin <avn0007@auburn.edu>
2023-02-12 07:03:18 -0600

    Added remaining checkboxes
    Added remaining checkboxes + other odd work to get gitHub up to date

--- ss-tracker/client/src/Content/Sidebar.js
+++ ss-tracker/client/src/Content/Sidebar.js
@@ -14,6 +14,36 @@ class Sidebar extends Component {
             <label for="BrendaData"> Brenda</label><br></br>
             <input type="checkbox" id="BuddyData"></input>
             <label for="BuddyData"> Buddy</label><br></br>
+            <input type="checkbox" id="CarlData"></input>
+            <label for="CarlData"> Carl</label><br></br>
+            <input type="checkbox" id="CarolinaData"></input>
+            <label for="CarolinaData"> Carolina</label><br></br>
+            <input type="checkbox" id="DaymondData"></input>
+            <label for="DaymondData"> Daymond</label><br></br>
+            <input type="checkbox" id="EinsteinData"></input>
+            <label for="EinsteinData"> Einstein</label><br></br>
+            <input type="checkbox" id="HelenData"></input>
+            <label for="HelenData"> Helen</label><br></br>
+            <input type="checkbox" id="JaneData"></input>
+            <label for="JaneData"> Jane</label><br></br>
+            <input type="checkbox" id="JaxData"></input>
+            <label for="JaxData"> Jax</label><br></br>
+            <input type="checkbox" id="LexiData"></input>
+            <label for="LexiData"> Lexi</label><br></br>
+            <input type="checkbox" id="Louise"></input>
+            <label for="Louise"> Louise</label><br></br>
+            <input type="checkbox" id="MadibaData"></input>
+            <label for="MadibaData"> Madiba</label><br></br>
+            <input type="checkbox" id="MayaData"></input>
+            <label for="MayaData"> Carl</label><br></br>
+            <input type="checkbox" id="OprahData"></input>
+            <label for="OprahData"> Oprah</label><br></br>
+            <input type="checkbox" id="OscarData"></input>
+            <label for="OscarData"> Oscar</label><br></br>
+            <input type="checkbox" id="SiegiData"></input>
+            <label for="SiegiData"> Siegi</label><br></br>
+            <input type="checkbox" id="SophieData"></input>
+            <label for="SophieData"> Sophie</label><br></br>
         </p>
       </div>
     );
