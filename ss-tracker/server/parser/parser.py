890d43f05fb4ad4704a8655d4b4f0bd71e9d07b5
Alexander V Nolin <avn0007@auburn.edu>
2023-02-12 02:54:09 -0600

    Finalized updates on parser
    changed code to allow ease of use for uploading data

--- ss-tracker/server/parser/parser.py
+++ ss-tracker/server/parser/parser.py
@@ -1,31 +1,32 @@
 import json
-file1 = open('/Users/alexnolin/Desktop/HackAThon/Data/BrendaData.csv', 'r')
-file = '/Users/alexnolin/Desktop/HackAThon/Data/BrendaData.csv'
+sharkName = 'Brenda'
+file1 = open('/Users/alexnolin/Desktop/HackAThon/Data/' + sharkName +'Data.csv', 'r')
+file = '/Users/alexnolin/Desktop/HackAThon/Data/' + sharkName +'Data.csv'
 allLines = []
 Lines = file1.readlines()
 for line in Lines:
-        allLines.append(line.strip().split(','))
+        allLines.append(line.strip().lstrip('\ufeff').split(','))
 fields = allLines[0]
 
-dict1 = {}
+print(sharkName)
 
 with open(file) as fh:
         iter1 = 1
-        firstTime = 0
+        time = 0
         for line in fh:
-                if(firstTime == 1):
+                dict1 = {}
+                if(time > 0 and time < 21):
                         description = list(line.strip().split(","))
-                        sno = 'shark' + str(iter1)
+                        sno = sharkName.lower() + str(iter1)
                         iter2 = 0
                         dict2 = {}
                         while iter2<len(fields):
                                 dict2[fields[iter2]]= description[iter2]
                                 iter2 = iter2 + 1
                         dict1[sno] = dict2
+                        out_file = open("/Users/alexnolin/Desktop/Data1/" + sharkName +"/" + sharkName + "Result" + str(iter1) + ".json", "w", encoding = 'utf-8-sig')
+                        json.dump(dict1, out_file, indent = 0)
                         iter1 = iter1 +1
-                firstTime = 1
+                        out_file.close()
+                time += 1
                 
-out_file = open("/Users/alexnolin/Desktop/HackAThon/Data/BrendaResult.json", "w", encoding = 'utf-8-sig')
-json.dump(dict1, out_file, indent = 0)
-out_file.close()
-
